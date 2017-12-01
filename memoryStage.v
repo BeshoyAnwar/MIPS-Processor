bed44b91773211f91c292464bec307a1b3e0b912
Menna Mohamed Abdallah <32853257+MennaAbdallah@users.noreply.github.com>
2017-12-01 14:08:57 +0200

    Update memoryStage.v

--- memoryStage.v
+++ memoryStage.v
@@ -1,6 +1,6 @@
-module dataMemory (input wrEnable, input [4:0] wrAddress, input [31:0] wrData, input rdEnable, input [4:0] rdAddress, output [31:0] rdData, input clk);
+module dataMemory (input wrEnable, input [9:0] wrAddress, input [31:0] wrData, input rdEnable, input [9:0] rdAddress, output [31:0] rdData, input clk);
 
-	reg [31:0] memFile [0:31];
+	reg [31:0] memFile [0:1023];
 
 	assign rdData = rdEnable? memFile[rdAddress] : 0;
 
@@ -19,23 +19,17 @@ wire MemToReg=EXMEMReg[72];
 wire MemWrite=EXMEMReg[73];
 wire MemRead=EXMEMReg[71];
 wire [4:0] WriteRegister = EXMEMReg[68:64];
-wire [4:0] RegLw=;
-wire [4:0] RegSw;
-wire WriteEnable;
-wire MemToRegWrite;
 
-wire [31:0] inCh0; 
-wire [31:0] inCh1;
 wire [31:0] selData;
 
 always @(posedge clk) begin
 MEMWBReg = {MemToReg,memoryAddress,RegWrite,WriteRegister,rdData};
 //$display ("memtoreg: %d memoryaddress: %d regwrite: %d writeregister: %d rddata: %d", MemToReg,memoryAddress,RegWrite,WriteRegister,rdData);
 end
-dataMemory d1(MemWrite, memoryAddress[4:0], WriteData, MemRead,
- memoryAddress[4:0],rdData, clk);
-forward f2(RegLw,RegSw,WriteEnable,MemToRegWrite,forwardF);
-mux (forwardF,inCh0, inCh1, selData)
+dataMemory d1(MemWrite, memoryAddress[9:0], selData, MemRead,
+ memoryAddress[9:0],rdData, clk);
+forward f2(MEMWBReg[36:32],WriteRegister,MEMWBReg[37],MEMWBReg[70],EXMEMReg[73],forwardF);
+mux m2(forwardF,WriteData, rdData, selData);
 endmodule
 
 module memoryStageTb;
@@ -76,6 +70,23 @@ EXMEMReg[71]=1'b1;//MemRead
 EXMEMReg[72]=1'b1;//MemToReg
 EXMEMReg[73]=1'b1;//MemWrite
 EXMEMReg[74]=1'b0;//RegWrite
+#10 //lw
+EXMEMReg[31:0]=32'h01011111;//result of ALU
+EXMEMReg[63:32]=32'h01010111;//ReadData2
+EXMEMReg[68:64]=5'b11000;//WriteReg
+EXMEMReg[71]=1'b1;//MemRead
+EXMEMReg[72]=1'b1;//MemToReg
+EXMEMReg[73]=1'b1;//MemWrite
+EXMEMReg[74]=1'b1;//RegWrite
+#10 //sw
+EXMEMReg[31:0]=32'h01010000;//result of ALU
+EXMEMReg[63:32]=32'h01010100;//ReadData2
+EXMEMReg[68:64]=5'b11000;//WriteReg
+EXMEMReg[71]=1'b1;//MemRead
+EXMEMReg[72]=1'b0;//MemToReg
+EXMEMReg[73]=1'b1;//MemWrite
+EXMEMReg[74]=1'b0;//RegWrite
 end
 memoryStage m1(clk,EXMEMReg,MEMWBReg);
 endmodule
+vv
