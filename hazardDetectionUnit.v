2036fc97740820cc3219a7e71efcfc855a87b56a
Peter Rateb <31657506+peterrateb@users.noreply.github.com>
2017-12-03 01:37:52 +0200

    Update hazardDetectionUnit.v

--- hazardDetectionUnit.v
+++ hazardDetectionUnit.v
@@ -1,10 +1,12 @@
-module hazardDetectionUnit ( input [4:0]IFIDRegrs , input [4:0]IFIDRegrt , input [4:0]IDEXRegrt , input [5:0] opcode , output reg controlMUX , output reg IFIDRegHOLD , output reg pcHOLD ,output reg IFflush);
+module hazardDetectionUnit ( input [4:0]IFIDRegrs , input [4:0]IFIDRegrt , input [4:0]IDEXRegrt ,input [4:0]IDEXRegrd ,input [4:0] EXMERegwriteReg,
+ input [5:0] IFIDopcode ,input [5:0] IDEXopcode,input IDEXregwrite ,input IDEXregdst,input EXMEMregwrite , input EXMEMmemread ,
+ output reg IFIDRegHOLD , output reg pcHOLD ,output reg IFflush);
 
-parameter beqOPcode = 6'b000100 , lwOPcode = 6'b100011 ;
+parameter beqOPcode = 6'b000100 ;
+parameter lwOPcode = 6'b100011 , R_format = 6'b000000 , swOPcode = 6'b101011 ;
 
 initial
 begin
-controlMUX =0;
 IFIDRegHOLD =0;
 pcHOLD =0;
 IFflush=0;
@@ -12,24 +14,40 @@ end
 
 always@(*)
 begin
-if (opcode==lwOPcode && ((IDEXRegrt == IFIDRegrs) || (IDEXRegrt == IFIDRegrt)))
+if (IFIDopcode==R_format &&((IDEXRegrt == IFIDRegrs) || (IDEXRegrt == IFIDRegrt)) && IDEXregwrite==1 && IDEXregdst ==0) //stall after lw instruction
 begin
-controlMUX=1;
 IFIDRegHOLD=1;
 pcHOLD=1;
 IFflush=1;
 end
-else if (opcode==beqOPcode && ((IDEXRegrt == IFIDRegrs) || (IDEXRegrt == IFIDRegrt)))
+else if (IFIDopcode==swOPcode &&((IDEXRegrt == IFIDRegrs) ) && IDEXregwrite==1 && IDEXregdst ==0) //stall sw dependency on lw instruction
 begin
-controlMUX=1;
+IFIDRegHOLD=1;
+pcHOLD=1;
+IFflush=1;
+end
+else if (IFIDopcode==beqOPcode && ((IDEXRegrd == IFIDRegrs) || (IDEXRegrd == IFIDRegrt)) && IDEXregwrite==1 && IDEXregdst ==1) //@ beq stall after R-format instruction 
+begin
+IFIDRegHOLD=1;
+pcHOLD=1;
+IFflush=1;
+end
+else if (IFIDopcode==beqOPcode && ((EXMERegwriteReg == IFIDRegrs) || (EXMERegwriteReg == IFIDRegrt)) && EXMEMregwrite==1 && EXMEMmemread ==1) //@beq second stall after lw instruction
+begin
+IFIDRegHOLD=1;
+pcHOLD=1;
+IFflush=1;
+end
+/*else if (IDEXopcode==beqOPcode && ((IDEXRegrt == IFIDRegrs) || (IDEXRegrt == IFIDRegrt)) ) //control hazard
+begin
 IFIDRegHOLD=0;
 pcHOLD=0;
 IFflush=1;
-end
+end*/
 
 else
 begin
-controlMUX=0;
+//controlMUX=0;
 IFIDRegHOLD=0;
 pcHOLD=0;
 IFflush=0;
@@ -43,11 +61,21 @@ endmodule
 
 module hazardUnitTB ;
 
-reg [4:0]IFIDRegrs;
-reg [4:0]IFIDRegrt;
-reg [4:0]IDEXRegrt;
-reg [5:0] OPcode ;
-wire controlMUX ;
+reg [4:0]IFIDRegrs ;
+reg [4:0]IFIDRegrt ;
+reg [4:0]IDEXRegrt ;
+reg [4:0]IDEXRegrd ;
+reg [4:0] EXMERegwriteReg;
+reg [5:0] IFIDopcode ;
+reg [5:0] IDEXopcode;
+reg IDEXregwrite ;
+reg IDEXregdst;
+reg EXMEMregwrite ;
+reg EXMEMmemread ;
+
+
+
+
 wire IFIDRegHOLD;
 wire pcHOLD ;
 wire IFflush ;
@@ -55,42 +83,64 @@ wire IFflush ;
 initial
 begin
 
-$monitor ("OPcode =%d ,IFIDRegrs=%d , IFIDRegrt=%d , IDEXRegrt=%d , controlMUX=%d , IFIDRegHOLD=%d ,  pcHOLD=%d , IFflush=%d" , OPcode,IFIDRegrs , IFIDRegrt , IDEXRegrt ,  controlMUX , IFIDRegHOLD ,  pcHOLD , IFflush );
+$monitor ("IFIDRegrs=%d IFIDRegrt=%d ; IDEXRegrt=%d; IDEXRegrd=%d,EXMERegwriteReg=%d,IFIDopcode=%d,IDEXopcode= %d,IDEXregwrite = %d,IDEXregdst=%d,EXMEMregwrite =%d,EXMEMmemread=%d \n IFIDRegHOLD=%d ,  pcHOLD=%d , IFflush=%d " , IFIDRegrs,IFIDRegrt,IDEXRegrt,IDEXRegrd,EXMERegwriteReg,IFIDopcode,IDEXopcode,IDEXregwrite,IDEXregdst,EXMEMregwrite,EXMEMmemread, IFIDRegHOLD ,  pcHOLD , IFflush );
 
 #10
-IFIDRegrs=0;
-IFIDRegrt=5;
+IFIDRegrs=7 ;
+IFIDRegrt=8 ;
+IDEXRegrt=14;
+IDEXRegrd=22;
+EXMERegwriteReg=1;
+IFIDopcode= 6'b000100 ; //beq
+IDEXopcode= 0;
+IDEXregwrite = 1;
+IDEXregdst=0;
+EXMEMregwrite =1;
+EXMEMmemread=0;
+
+
+#10
+IFIDRegrs=7 ;
+IFIDRegrt=8 ;
+IDEXRegrt=7;
+IDEXRegrd=22;
+EXMERegwriteReg=15;
+IFIDopcode= 0 ; //rformat
+IDEXopcode= 6'b100011 ; //lw
+IDEXregwrite = 1;
+IDEXregdst=0;
+EXMEMregwrite =1;
+EXMEMmemread=0;
+
+#10
+IFIDRegrs=7 ;
+IFIDRegrt=8 ;
+IDEXRegrt=7;
+IDEXRegrd=22;
+EXMERegwriteReg=15;
+IFIDopcode= 4 ; //beq
+IDEXopcode= 35 ; //lw
+IDEXregwrite = 1;
+IDEXregdst=0;
+EXMEMregwrite =1;
+EXMEMmemread=0;
+#10
+IFIDRegrs=1 ;
+IFIDRegrt=8 ;
 IDEXRegrt=1;
-OPcode=0;
+IDEXRegrd=22;
+EXMERegwriteReg=0;
+IFIDopcode= 0 ; //rformat
+IDEXopcode= 35 ; //lw
+IDEXregwrite = 1;
+IDEXregdst=0;
+EXMEMregwrite =1;
+EXMEMmemread=1;
 
-
-#10
-IFIDRegrs=5;
-IFIDRegrt=7;
-IDEXRegrt=7;
-OPcode=35;
-
-#10
-IFIDRegrs=5;
-IFIDRegrt=7;
-IDEXRegrt=7;
-OPcode=4;
-
-
-#10
-IFIDRegrs=10;
-IFIDRegrt=9;
-IDEXRegrt=10;
-OPcode=0;
-
-
-#10
-IFIDRegrs=10;
-IFIDRegrt=9;
-IDEXRegrt=10;
-OPcode=4;
 end
 
-hazardDetectionUnit  hazardUnitTEST ( IFIDRegrs , IFIDRegrt , IDEXRegrt , OPcode , controlMUX ,IFIDRegHOLD , pcHOLD ,IFflush);
-
+//hazardDetectionUnit  hazardUnitTEST ( IFIDRegrs , IFIDRegrt , IDEXRegrt , OPcode , IFIDRegHOLD , pcHOLD ,IFflush);
+hazardDetectionUnit  hazardUnitTEST (IFIDRegrs ,IFIDRegrt ,IDEXRegrt ,IDEXRegrd ,EXMERegwriteReg,
+IFIDopcode ,IDEXopcode,IDEXregwrite , IDEXregdst, EXMEMregwrite , EXMEMmemread , 
+IFIDRegHOLD , pcHOLD , IFflush);
 endmodule
