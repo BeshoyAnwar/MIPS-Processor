32c7fdcfb3bcf8e82e3482086751d11d13818bf3
Menna Mohamed Abdallah <32853257+MennaAbdallah@users.noreply.github.com>
2017-12-03 15:12:34 +0200

    Update memoryStage.v

--- memoryStage.v
+++ memoryStage.v
@@ -21,15 +21,15 @@ wire MemRead=EXMEMReg[71];
 wire [4:0] WriteRegister = EXMEMReg[68:64];
 
 wire [31:0] selData;
-
+wire [1:0] forwardF;
 always @(posedge clk) begin
 MEMWBReg = {MemToReg,memoryAddress,RegWrite,WriteRegister,rdData};
-//$display ("memtoreg: %d memoryaddress: %d regwrite: %d writeregister: %d rddata: %d", MemToReg,memoryAddress,RegWrite,WriteRegister,rdData);
+
 end
 dataMemory d1(MemWrite, memoryAddress[9:0], selData, MemRead,
  memoryAddress[9:0],rdData, clk);
-forward f2(MEMWBReg[36:32],WriteRegister,MEMWBReg[37],MEMWBReg[70],forwardF);
-mux m2(forwardF,WriteData, rdData, selData);
+forward f2(MEMWBReg[36:32],WriteRegister,MEMWBReg[37],EXMEMReg[73],MEMWBReg[70],forwardF);
+mux3To1 m2(forwardF, WriteData,rdData , MEMWBReg[69:38], selData);
 endmodule
 
 module memoryStageTb;
@@ -45,43 +45,60 @@ initial
 begin 
 EXMEMReg = {75 {1'b0}};
 clk = 0;
-$monitor ($time,"MemToReg %b memoryAddress %h RegWrite %d WriteRegister %b rdData %h MEMWBReg %b",MemToReg ,memoryAddress ,RegWrite ,WriteRegister ,rdData,MEMWBReg);
-#10
-EXMEMReg[31:0]=32'h00000000;//result of ALU
-EXMEMReg[63:32]=32'h01010101;//ReadData2
-EXMEMReg[68:64]=5'b11011;//WriteReg
-EXMEMReg[71]=1'b1;//MemRead
-EXMEMReg[72]=1'b1;//MemToReg
-EXMEMReg[73]=1'b0;//MemWrite
-EXMEMReg[74]=1'b0;//RegWrite
-#10
-EXMEMReg[31:0]=32'h11111111;//result of ALU
-EXMEMReg[63:32]=32'h01010101;//ReadData2
-EXMEMReg[68:64]=5'b11111;//WriteReg
-EXMEMReg[71]=1'b0;//MemRead
-EXMEMReg[72]=1'b1;//MemToReg
+$monitor ($time,"MemToReg %b memoryAddress %h RegWrite %d WriteRegister %b rdData %h "/*MEMWBReg %b*/,MemToReg ,memoryAddress ,RegWrite ,WriteRegister ,rdData/*,MEMWBReg*/);
+/*
+EXMEMReg[31:0]=32'h0000000c;//result of ALU
 EXMEMReg[73]=1'b1;//MemWrite
-EXMEMReg[74]=1'b0;//RegWrite
-#10
-EXMEMReg[31:0]=32'h01011111;//result of ALU
-EXMEMReg[63:32]=32'h01010101;//ReadData2
-EXMEMReg[68:64]=5'b11011;//WriteReg
-EXMEMReg[71]=1'b1;//MemRead
-EXMEMReg[72]=1'b1;//MemToReg
-EXMEMReg[73]=1'b1;//MemWrite
-EXMEMReg[74]=1'b0;//RegWrite
+EXMEMReg[63:32]=32'h01010111;//ReadData2
 #10 //lw
-EXMEMReg[31:0]=32'h01011111;//result of ALU
+EXMEMReg[31:0]=32'h0000000c;//result of ALU
 EXMEMReg[63:32]=32'h01010111;//ReadData2
 EXMEMReg[68:64]=5'b11000;//WriteReg
 EXMEMReg[71]=1'b1;//MemRead
 EXMEMReg[72]=1'b1;//MemToReg
 EXMEMReg[73]=1'b0;//MemWrite
 EXMEMReg[74]=1'b1;//RegWrite
+*/
 #10 //sw
-EXMEMReg[31:0]=32'h10000000;//result of ALU
+EXMEMReg[31:0]=32'h0000000c;//result of ALU
 EXMEMReg[63:32]=32'h01010100;//ReadData2
-EXMEMReg[68:64]=5'b11000;//WriteReg
+EXMEMReg[68:64]=5'b11100;//WriteReg
+EXMEMReg[71]=1'b0;//MemRead
+EXMEMReg[72]=1'b0;//MemToReg
+EXMEMReg[73]=1'b1;//MemWrite
+EXMEMReg[74]=1'b0;//RegWrite
+
+#10 //lw
+EXMEMReg[31:0]=32'h0000000c;//result of ALU
+EXMEMReg[63:32]=32'h00000011;//ReadData2
+EXMEMReg[68:64]=5'b11100;//WriteReg
+EXMEMReg[71]=1'b1;//MemRead
+EXMEMReg[72]=1'b1;//MemToReg
+EXMEMReg[73]=1'b0;//MemWrite
+EXMEMReg[74]=1'b1;//RegWrite
+
+#10 //sw
+EXMEMReg[31:0]=32'h00000018;//result of ALU
+EXMEMReg[63:32]=32'h01010100;//ReadData2
+EXMEMReg[68:64]=5'b11100;//WriteReg
+EXMEMReg[71]=1'b0;//MemRead
+EXMEMReg[72]=1'b0;//MemToReg
+EXMEMReg[73]=1'b1;//MemWrite
+EXMEMReg[74]=1'b0;//RegWrite
+
+#10 //add
+EXMEMReg[31:0]=32'h08438433;//result of ALU
+EXMEMReg[63:32]=32'h8f3da232;//ReadData2
+EXMEMReg[68:64]=5'b11100;//WriteReg
+EXMEMReg[71]=1'b0;//MemRead
+EXMEMReg[72]=1'b0;//MemToReg
+EXMEMReg[73]=1'b0;//MemWrite
+EXMEMReg[74]=1'b1;//RegWrite
+
+#10 //sw
+EXMEMReg[31:0]=32'h00000019;//result of ALU
+EXMEMReg[63:32]=32'h01354440;//ReadData2
+EXMEMReg[68:64]=5'b11100;//WriteReg
 EXMEMReg[71]=1'b0;//MemRead
 EXMEMReg[72]=1'b0;//MemToReg
 EXMEMReg[73]=1'b1;//MemWrite
