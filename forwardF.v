33ea5b8f3cad9378a50b24c70fcd4195523ca40f
Menna Mohamed Abdallah <32853257+MennaAbdallah@users.noreply.github.com>
2017-12-03 02:02:26 +0200

    Update forwardF.v

--- forwardF.v
+++ forwardF.v
@@ -1,39 +1,35 @@
-module forward(input RegWrite,input [4:0] RegLw,input[4:0] RegSw,input WriteEnable,input memWrite,output forwardF);
+module forward(input [4:0] RegLw,input[4:0] RegSw,input WriteEnable,input memWrite,output forwardF);
 
-assign forwardF=((WriteEnable==1'b1)&&((RegWrite==1'b0)||(RegLw==RegSw))&&(RegSw!=5'b00000)&&(memWrite==1'b1))?1'b1:1'b0;
+	assign forwardF=((WriteEnable==1'b1)&&(RegLw==RegSw)&&(RegLw!=5'b00000)&&(RegSw!=5'b00000)&&(memWrite==1'b1))?1'b1:1'b0;
 always @(forwardF)
 $display("forward");
 endmodule
 
 module forwardtb;
-reg[4:0] RegLw;reg[4:0] RegSw ;reg WriteEnable;reg RegWrite ;reg memWrite;wire forwardF;
+reg[4:0] RegLw;reg[4:0] RegSw ;reg WriteEnable;reg memWrite;wire forwardF;
 initial
 begin
-$monitor($time,"RegLw %b RegSw %b WriteEnable %b RegWrite %b memWrite %b forwardF %b",RegLw,RegSw ,WriteEnable,RegWrite ,memWrite, forwardF);
+$monitor($time,"RegLw %b RegSw %b WriteEnable %b MemToRegWrite %b memWrite %b forwardF %b",RegLw,RegSw ,WriteEnable,MemToRegWrite,memWrite, forwardF);
 #10
 RegLw=5'b10101;
 RegSw =5'b10101;
 WriteEnable=1'b1;
-RegWrite =1'b1;
 memWrite=1'b1;
 #10
 RegLw=5'b10100;
 RegSw =5'b10101;
 WriteEnable=1'b1;
-RegWrite =1'b1;
 memWrite=1'b1;
 #10
 RegLw=5'b10101;
 RegSw =5'b10101;
 WriteEnable=1'b0;
-RegWrite =1'b1;
 memWrite=1'b0;
 #10
 RegLw=5'b10101;
 RegSw =5'b10101;
 WriteEnable=1'b1;
-RegWrite =1'b0;
 memWrite=1'b1;
 end
- forward f1(RegWrite,RegLw,RegSw,WriteEnable, memWrite, forwardF);
+forward f1(RegLw,RegSw,WriteEnable,memWrite,forwardF);
 endmodule
